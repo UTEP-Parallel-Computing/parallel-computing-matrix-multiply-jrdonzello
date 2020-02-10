2c50cf38b27eec8429134adb9a4b04fb4e2d2933
jrdonzello <jrdonzello@miners.utep.edu>
2020-02-09 17:58:42 -0700

    Assignment 1 Part 2 commit

--- ReportJustenDonzello.md
+++ ReportJustenDonzello.md
@@ -1,3 +1,21 @@
+Assignment 1 Part 2 Report: Implementing Parallel
+The two main problems that occurred when working on this was where in the code to actually implement the parallel part and what elements need to be included in the parallel part. There are no bugs to my knowledge and everything should be working correctly.
+
+This assignment took nearly 3 hours, half working on implementation and testing the program and the other half was for running the program to generate results.
+
+Performance Stats: (Testing with size=1000)
+1 thread : 0.72~0.79
+2 threads : 0.35~0.40
+4 threads : 0.193~0.22
+8 threads : 0.10~0.12
+
+By doubling the amount of threads, the amount of time is almost half of the previous time elapsed. This correlates with how I originally thought the program would react and logically/theoretically makes sense as well.
+
+dumpCPUInfo.sh output:
+model name	: Intel(R) Core(TM) i5-7360U CPU @ 2.30GHz
+      1       9      54
+
+
 Assignment 1 Report: Serial Matrix Multiplication
 
 This Python script runs the standard way by calling the main file's name, 'python3 matrixMain.py'.
--- matrixMain.py
+++ matrixMain.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
-import matrixUtils, time
+import matrixUtils, time, pymp
 
 class MatrixMain():
     def __init__(self):
         value = input("Input custom size and value? Or use standard size=1024, value=1? \nExample Input: 10 5\n")
         if value is not "":
             value = value.split(" ")
-            self.matrix = matrixUtils.genMatrix(int(value[0]), int(value[1]))
+            self.matrix = pymp.shared.array((int(value[0]),int(value[0])), dtype='intc')
+            self.assignVals(value[1])
+            #self.matrix = matrixUtils.genMatrix(int(value[0]), int(value[1]))
         else:
-            self.matrix = matrixUtils.genMatrix()
+            self.matrix = pymp.shared.array((1024,1024),dtype='intc')
+            self.assignVals(1)
+            #self.matrix = matrixUtils.genMatrix()
+
+    def assignVals(self, value):
+        for x in range(len(self.matrix)):
+            for y in range(len(self.matrix[x])):
+                self.matrix[x][y] = value
 
     def main(self):
         start = time.process_time()
@@ -17,9 +26,11 @@ class MatrixMain():
         print('{} {}'.format("Computation Time:", end))
 
     def multiplyMatrix(self):
-        for x in range(len(self.matrix)):
-            for y in range(len(self.matrix[x])):
-                self.matrix[x][y] *= self.matrix[x][y]
+        with pymp.Parallel(8) as p:
+            print('Thread {} of {}'.format(p.thread_num,p.num_threads))
+            for x in p.range(0,len(self.matrix)):
+                for y in range(0,len(self.matrix[x])):
+                    self.matrix[x][y] *= self.matrix[x][y]
 
     def printSubMatrix(self):
         matrixUtils.printSubarray(self.matrix, 10)
@@ -38,4 +49,5 @@ class MatrixMain():
 if __name__ == '__main__':
     matrix = MatrixMain()
     matrix.main()
-    matrix.printSubMatrix()
+    #matrix.printSubMatrix()
+    matrix.printMatrix()
